d46936a765bb680913b03b9accd8e4136d5b9e5a
Allen Abraham <allen61310@gmail.com>
2024-08-22 10:56:09 -0400

    more config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://motowntech.com',
-  base: '/motown-lp',
+  base: '/',
   build: {
     assets: 'assets'
   },
